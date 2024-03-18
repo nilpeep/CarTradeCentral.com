f24487beceb033e8fa481a30aaae4dc877b7bd83
nilpeep <niluferk038@gmail.com>
2024-03-18 15:23:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+"use strict";
+const express = require('express')
+const app = express()
+
+/* DB connection  */
+
+const mongoose = require('mongoose')
+
+const MONGODB = process.env.MONGO
+
+mongoose.connect('mongodb://localhost:27017/sahibinden')
+.then(()=> console.log('mongoDB connected'))
+.catch((err)=> console.log(err))
+
+
+const PORT=8000
+const HOST="localhost"
+
+app.all('/',(req,res)=>{
+    res.send('welcome')
+})
+
+const carSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    car_make: String,
+    car_model: String,
+    car_year: Number,
+    car_vin: String,
+    purchase_price: Number,
+    sale_price: Number,
+    stock_quantity: Number,
+    location: String,
+    last_updated: Date
+  });
+
+const Car = mongoose.model('cars', carSchema);
+
+app.get('/cars', async (req, res) => {
+    try {
+      const cars = await Car.find(); // Tüm arabaları çek
+      res.json(cars); // Araba listesini JSON formatında döndür
+    } catch (err) {
+      res.status(500).send({ message: err.message }); // Hata durumunda 500 durum kodu ile hata mesajını döndür
+    }
+  });
+
+app.listen(PORT,()=> console.log(` Server Running on http://${HOST}:${PORT}`))
