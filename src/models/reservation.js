8f598431e30c61179eb9aa2e0dc12da4a5b58d35
nilpeep <niluferk038@gmail.com>
2024-04-01 22:27:16 +0300

    reservation model

--- src/models/reservation.js
+++ src/models/reservation.js
@@ -18,3 +18,45 @@ const { mongoose } = require('../configs/dbConnection')
 }
 /* ------------------------------------------------------- */
 // Reservation Model:
+
+const reservationSchema = new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    carId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Car',
+        required: true
+    },
+    startDate:{
+        type: Date,
+        required: true
+    },
+    endDate:{
+        type: Date,
+        required: true
+    },
+    amount:{
+        type: Number,
+        required:true
+    },
+    createdId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+
+    },
+    updatedId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+
+}, {
+    collection: 'reservations',
+    timestamps: true
+})
+
+module.exports = mongoose.model('Reservation', reservationSchema)
